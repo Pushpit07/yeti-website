e874936dc77e7dee0bffb70a9614dde6393d02d4
Sanchitj23 <sanchit.jain@yeti-dresden.org>
2025-12-05 16:21:34 +0100

    added firebase config

---
+++ lib/firebase.ts
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBccsJF9WcmEzQPiAF-8Cfw1ThY8HHHWso",
+    authDomain: "yeti-dresden.firebaseapp.com",
+    projectId: "yeti-dresden",
+    storageBucket: "yeti-dresden.firebasestorage.app",
+    messagingSenderId: "5223299706",
+    appId: "1:5223299706:web:f50b651514e596c49e19c3",
+    measurementId: "G-7ZZ9BJPPXS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Analytics conditionally (safely)
+let analytics;
+if (typeof window !== "undefined") {
+    isSupported().then((supported) => {
+        if (supported) {
+            analytics = getAnalytics(app);
+        }
+    });
+}
+
+export { app, analytics };
